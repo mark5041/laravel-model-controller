ae1a605408d2681cdf4c2e8a254e585c3952b432
mark5041 <mark.beloso5041@gmail.com>
2022-02-17 16:44:30 +0100

    end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,22 @@
-@extends('layouts.app')
+
+@extends('layouts.base')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="container">
+        <div class="row gy-3">
+            @foreach ($movies as $element)
+            <div class="col-4" p-2">
+                
+                <div class="card p-2">
+                    <h2 class="text-center px-4 py-3">{{ $element->title }}</h2>
+                    <span>Original title: {{ $element->original_title }}</span>
+                    <span>Language: {{ $element->nationality }}</span>
+                    <span>Date: {{ $element->date }}</span>
+                    <span>Vote: {{ $element->vote }}</span>
                 </div>
+
             </div>
+            @endforeach
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'PageController@index');
